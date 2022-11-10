4be1833dcd3d1a02db8782a2ea6ff093c3e6d143
alexCoding42 <alexandre.cisse.ac@gmail.com>
2022-11-10 13:38:04 +0100

    chore: :building_construction: add external libraries

--- App.tsx
+++ App.tsx
@@ -1,8 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { NhostApolloProvider } from '@nhost/react-apollo';
+import { NhostClient, NhostReactProvider } from '@nhost/react';
+import Storage from '@react-native-async-storage/async-storage';
 
 import useCachedResources from './src/hooks/useCachedResources';
 import Navigation from './src/navigation';
+
+const nhost = new NhostClient({
+  subdomain: 'pyfkyftcjbknfrcbvxtw',
+  region: 'eu-west-2',
+  clientStorageType: 'react-native',
+  clientStorage: Storage,
+});
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
@@ -11,10 +21,14 @@ export default function App() {
     return null;
   } else {
     return (
-      <SafeAreaProvider>
-        <Navigation />
-        <StatusBar style='light' />
-      </SafeAreaProvider>
+      <NhostReactProvider nhost={nhost}>
+        <NhostApolloProvider nhost={nhost}>
+          <SafeAreaProvider>
+            <Navigation />
+            <StatusBar style='light' />
+          </SafeAreaProvider>
+        </NhostApolloProvider>
+      </NhostReactProvider>
     );
   }
 }
